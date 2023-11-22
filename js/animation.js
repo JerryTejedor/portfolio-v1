85299c9bfa68bdb8614db9dbe1a361d2e3437a3c
JerryPadilla <jerrypadillatejedor@gmail.com>
2023-11-22 18:37:26 -0500

    first commit

---
+++ js/animation.js
@@ -0,0 +1,23 @@
+ScrollReveal().reveal('.header',{
+    duration: 3000,
+    origin:'bottom',
+    distance:'-100px',
+});
+
+// reveal('.main',{
+//     duration: 3000,
+//     origin: 'bottom',
+//     distance:'-100px',
+// });
+
+ScrollReveal().reveal('.main',{
+    duration: 4000,
+    origin:'bottom',
+    distance:'-100px',
+});
+
+ScrollReveal().reveal('.section__about',{
+    duration: 3000,
+    origin:'left',
+    distance:'800px',
+});
